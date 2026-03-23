6db586524d7f55d1c44d2f8903b58aec13fd63b7
Piyush Tiwari <ithastobepiyush@gmail.com>
2026-03-23 17:10:22 +0530

    frontend button completed publish and save draft

--- src/components/blog-editor.component.jsx
+++ src/components/blog-editor.component.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import logo from "../imgs/logo.png"
 import AnimationWrapper from "../common/page-animation"
 import defaultBanner from '../imgs/blog-banner.png'
@@ -8,24 +8,32 @@ import {Toaster, toast} from 'react-hot-toast'
 import { EditorContext } from "../pages/editor.page"
 import EditorJS from "@editorjs/editorjs"
 import { tools } from "./tools.component"
+import axios from "axios"
+import { UserContext } from "../App"
 
 const BlogEditor = () => {
 
     // let blogBannerRef = useRef()
     let {blog, blog:{title, banner, content, tags, des}, setBlog, textEditor, setTextEditor, editorState, setEditorState } = useContext(EditorContext)
 
+    let { userAuth: { accessToken } } = useContext(UserContext)
+
+    let navigate = useNavigate()
+
     // console.log(blog);
 
     // useEffect hook from react
     useEffect(() => {
-        setTextEditor(
-            new EditorJS({
-                holder: "textEditor",
-                data: content,
-                tools: tools,
-                placeholder: "Let's start sharing"
-            })
-        )
+        if(!textEditor.isReady){
+            setTextEditor(
+                new EditorJS({
+                    holder: "textEditor",
+                    data: content,
+                    tools: tools,
+                    placeholder: "Let's start sharing"
+                })
+            )
+        }
     }, [])
     
 
@@ -104,6 +112,55 @@ const BlogEditor = () => {
 
     }
 
+    const handleSaveDraft = (e) => {
+        
+        if(e.target.className.includes("disable")){
+            return
+        }
+
+        if(!title.length){
+            return toast.error("Write blog title before before saving it to draft")
+        }
+
+        let loadingToast = toast.loading("Saving Draft...")
+
+        e.target.classList.add('disable')
+
+
+        if(textEditor.isReady){
+            textEditor.save().then(content => {
+                let blogObj = { title, des, banner, tags, content, draft: true }
+                axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/create-blog", blogObj, {
+                    headers : {
+                        "Authorization" : `Bearer ${accessToken}`
+                }
+                })
+                .then(() => {
+                
+                e.target.classList.remove('disable')
+        
+                toast.dismiss(loadingToast)
+                toast.success("Saved 👍🏻")
+        
+                setTimeout(() => {
+                    navigate("/")
+                }, 500)
+        
+                })
+                .catch(( { response } ) => {
+                e.target.classList.remove('disable')
+                toast.dismiss(loadingToast)
+        
+                return toast.error(response.data.error)
+                })
+                    
+
+                
+            })
+        }
+
+    }
+
     return(
         <>
 
@@ -124,10 +181,13 @@ const BlogEditor = () => {
                         className="btn-dark py-2"
                         onClick={handlePublishEvent}
                     >
-
                         Publish
                     </button>
-                    <button className="btn-light py-2">
+
+                    <button
+                        className="btn-light py-2"
+                        onClick={handleSaveDraft}
+                        >
                         Save Draft
                     </button>
                 </div>
--- src/components/publish-form.component.jsx
+++ src/components/publish-form.component.jsx
@@ -1,13 +1,24 @@
-import { Toaster } from "react-hot-toast"
+import toast, { Toaster } from "react-hot-toast"
 import AnimationWrapper from "../common/page-animation"
 import { useContext } from "react"
 import { EditorContext } from "../pages/editor.page"
+import Tag from "./tags.component"
+import axios from "axios"
+import { UserContext } from "../App"
+import { useNavigate } from "react-router-dom"
 
 
 const PublishForm = () => {
-
+  
   let characterlLimit = 200
-  let {blog, blog:{banner, title, tags, des}, setEditorState, setBlog} = useContext(EditorContext)
+  let tagLimit = 10
+  
+  // destructuring of context
+  let {blog, blog:{banner, title, tags, des, content}, setEditorState, setBlog} = useContext(EditorContext)
+  
+  let {userAuth: {accessToken} } = useContext(UserContext)
+  
+  let navigate = useNavigate()
 
   const handleCloseEvent = () =>{
     setEditorState("editor")
@@ -26,7 +37,83 @@ const PublishForm = () => {
     setBlog({ ...blog, des: input.value })
   }
 
+  const handleDesKeyDown = (e) => {
+    // console.log(e);
+    if(e.keyCode == 13){
+        e.preventDefault()
+      }        
+    }
+
+  const handleTagKeyDown = (e) => {
+    if(e.keyCode == 13 || e.keyCode == 188){
+      e.preventDefault()
+
+      let tag = e.target.value
+
+      if(tags.length < tagLimit){
+        if(!tags.includes(tag) && tag.length){
+          setBlog({...blog, tags: [ ...tags, tag ] })
+        }
+      } else{
+        toast.error(`You can add max ${tagLimit} tags`)
+      }
+      // for the tag input it will create inbox empty again
+      e.target.value = ""
+    }  
+  }
+
+
+
+  const publishBlog = (e) => {
+
+    if(e.target.className.includes("disable")){
+      return
+    }
+
+    if(!title.length){
+      return toast.error("Write blog title before publishing")
+    }
+    if(!des.length || !des.length > characterlLimit ){
+      return toast.error(`Description is necessary, Write within ${characterlLimit} characters to publish`)
+    }
+    if(!tags.length){
+      return toast.error("Enter at least 1 tag to help us rank your blog")
+    }
+
+    let loadingToast = toast.loading("Publishing.. .")
+
+    e.target.classList.add('disable')
+
+    let blogObj = {
+      title, des, banner, tags, content, draft: false
+    }
+    axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/create-blog", blogObj, {
+      headers : {
+        "Authorization" : `Bearer ${accessToken}`
+      }
+    })
+    .then(() => {
+      
+      e.target.classList.remove('disable')
+
+      toast.dismiss(loadingToast)
+      toast.success("Published 👍🏻")
+
+      setTimeout(() => {
+        navigate("/")
+      }, 500)
+
+    })
+    .catch(( { response } ) => {
+      e.target.classList.remove('disable')
+      toast.dismiss(loadingToast)
+
+      return toast.error(response.data.error)
+    })
+  }
+
   return (
+
     <AnimationWrapper>
 
       <section className="w-screen min-h-screen grid items-center lg:grid-cols-2 py-16 lg:gap-4">
@@ -74,10 +161,42 @@ const PublishForm = () => {
             maxLength={characterlLimit}  
             className="h-40 resize-none leading-7 input-box"
             onChange={handleBlogDesChange}
+            onKeyDown={handleDesKeyDown}
           >
           </textarea>
 
           <p className="mt-1 text-dark-grey text-sm text-right">{characterlLimit - des.length  } characters left</p>
+
+
+          {/* tags section */}
+          <p className="text-dark-grey mb-2 mt-9">Topics- Helps in searching and ranking your blog post</p>
+
+          {/* input for the tags */}
+          <div className="relative input-box pl-2 py-2 pb-4">
+
+            <input 
+              className="sticky input-box bg-white top-0 left-0 pl-4 mb-3 focus:bg-white"
+              
+              type="text"
+              placeholder="Topics" 
+              onKeyDown={handleTagKeyDown}
+            />
+
+            { 
+              tags.map((tag, i) => {
+                return <Tag tag={tag} tagIndex={i} key={i} />
+              })
+            }
+          </div>
+          
+          <p className="mt-1 text-dark-grey text-sm text-right">{tagLimit - tags.length  } tags left</p>
+
+          <button 
+            className="btn-dark px-8"
+            onClick={publishBlog}
+          >
+            Publish
+          </button>
 
         </div>
 
