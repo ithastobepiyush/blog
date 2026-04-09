803559d66db76f5863db0606e278d7bb4f806d4c
Piyush Tiwari <ithastobepiyush@gmail.com>
2026-04-09 12:05:42 +0530

    Homepage and pagination

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,8 +1,23 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import AnimationWrapper from "../common/page-animation"
 import InpageNavigation from "../components/inpage-navigation.component"
+import axios from "axios"
 
 const HomePage = () => {
+
+    const fetchlatestBlog = () => {
+        axios.get(import.meta.env.VITE_SERVER_DOMAIN + "/latest-blog")
+        .then(({ blogs }) => {
+            console.log(blogs)
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
+
+    useEffect(() => {
+        fetchlatestBlog()
+    }, [])
 
     return(
         <AnimationWrapper>
